690cf2a53be27b80f7d88aca01e8b95e637c125e
esteffin <enrylongo@gmail.com>
2017-05-11 19:32:56 +0100

    Added common function prelude (contains 2 vsersions, str_to_lower)

--- src/VACSAT/src/prelude.h
+++ src/VACSAT/src/prelude.h
@@ -7,15 +7,31 @@
 #define VACSAT_PRELUDE_H
 
 #include <set>
+#include <algorithm>
 
-template <typename T>
-static inline bool contains(const std::set<T>& set, const T& elem) {
-    return set.find(elem) != set.end();
-}
+namespace SMT {
 
-template<class InputIterator, class T>
-static inline bool contains(InputIterator first, InputIterator last, const T& val) {
-    return std::find(first, last, val) != last;
+    template <typename T>
+    static inline bool contains(const std::set<T> &set, const T &elem) {
+        return set.find(elem) != set.end();
+    }
+
+    template <class InputIterator, class T>
+    static inline bool contains(InputIterator first, InputIterator last, const T &val) {
+        return std::find(first, last, val) != last;
+    }
+
+//    static inline void str_to_lower(std::string &str) {
+//        std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+//    }
+
+    static inline std::string str_to_lower(const std::string &str) {
+        std::string res = "";
+        for (auto &&ch : str) {
+            res += std::tolower(ch);
+        }
+        return res;
+    }
 }
 
 #endif //VACSAT_PRELUDE_H
