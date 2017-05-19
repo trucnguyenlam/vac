3ff0bd7f67d0ef3965736768ef73083e921f188e
esteffin <enrylongo@gmail.com>
2017-05-19 16:05:34 +0100

    Added debug variables

---
+++ src/VACSAT/src/debug.h
@@ -0,0 +1,18 @@
+//
+// Created by esteffin on 18/05/17.
+//
+
+#ifndef VACSAT_DEBUG_H
+#define VACSAT_DEBUG_H
+
+namespace SMT {
+    namespace Debug {
+        static bool experimental = false;
+
+        static bool log = true;
+
+    }
+
+}
+
+#endif //VACSAT_DEBUG_H
