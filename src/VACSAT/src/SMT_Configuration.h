680916254dfecdc70115145f1ba8766f6eeafbd6
esteffin <enrylongo@gmail.com>
2017-05-25 18:54:11 +0100

    Added config file header. (Empty and not used for the moment)

---
+++ src/VACSAT/src/SMT_Configuration.h
@@ -0,0 +1,16 @@
+//
+// Created by esteffin on 25/05/17.
+//
+
+#ifndef VACSAT_SMT_CONFIGURATION_H
+#define VACSAT_SMT_CONFIGURATION_H
+
+class Logging {
+
+};
+
+class cfg {
+    
+};
+
+#endif //VACSAT_SMT_CONFIGURATION_H
