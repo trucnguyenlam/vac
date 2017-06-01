b336f630979e9246d1d3bd3e0b683b30f3b80e33
esteffin <enrylongo@gmail.com>
2017-06-01 12:07:48 +0100

    SMT_Analysis_old_style.cpp: renamed old analysis functions to avoid clashes. (TODO: we should support both parser for)

--- src/VACSAT/src/SMT_Analysis_old_style.cpp
+++ src/VACSAT/src/SMT_Analysis_old_style.cpp
@@ -10,7 +10,7 @@
 namespace SMT {
 
     template <typename TVar, typename TExpr>
-    void execute(AnalysisType analysis_type,
+    void execute_old(AnalysisType analysis_type,
                  const std::shared_ptr<SMTFactory<TVar, TExpr>>& solver,
                  const std::shared_ptr<arbac_policy>& policy, bmc_config config) {
         switch (analysis_type) {
@@ -29,17 +29,17 @@ namespace SMT {
         arbac_to_smt_bmc(solver, policy, config.rounds, config.steps, config.wanted_threads_count);
     };
 
-    void set_solver_execute(AnalysisType analysis_type, const std::shared_ptr<arbac_policy>& policy, const std::string& solver_name, bmc_config config) {
+    void set_solver_execute_old(AnalysisType analysis_type, const std::shared_ptr<arbac_policy>& policy, const std::string& solver_name, bmc_config config) {
 
         if (str_to_lower(solver_name) == str_to_lower(YicesSolver::solver_name())) {
             std::cout << "Using " << solver_name << " as backend" << std::endl;
             std::shared_ptr<SMTFactory<term_t, term_t>> solver(new YicesSolver());
-            execute(analysis_type,solver, policy, config);
+            execute_old(analysis_type,solver, policy, config);
         }
         else if (str_to_lower(solver_name) == str_to_lower(Z3Solver::solver_name())) {
             std::cout << "Using " << solver_name << " as backend" << std::endl;
             std::shared_ptr<SMTFactory<expr, expr>> solver(new Z3Solver());
-            execute(analysis_type,solver, policy, config);
+            execute_old(analysis_type,solver, policy, config);
         }
         else {
             std::cout << "Backend " << solver_name << " is not supported" << std::endl;
@@ -55,7 +55,7 @@ namespace SMT {
         build_config_array();
 
         std::shared_ptr<arbac_policy> policy(new arbac_policy(true));
-        set_solver_execute(analysis_type, policy, solver_name, config);
+        set_solver_execute_old(analysis_type, policy, solver_name, config);
 
 
         free_data();
