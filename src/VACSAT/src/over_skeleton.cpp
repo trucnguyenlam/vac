f0ee330d031eb80981347643dad465400da04a71
esteffin <enrylongo@gmail.com>
2017-12-30 12:11:06 +0000

    Last version before rollback (following Gennaro's specs)

--- src/VACSAT/src/over_skeleton.cpp
+++ src/VACSAT/src/over_skeleton.cpp
@@ -3,51 +3,125 @@
 //
 
 #include "over_skeleton.h"
+#include "prelude.h"
 
 namespace SMT {
 
-    template <typename LayerInfo, typename BlockInfo>
-    layer<LayerInfo, BlockInfo>::layer(int _depth,
-                 std::shared_ptr<LayerInfo> _infos,
-                 std::vector<std::shared_ptr<block<LayerInfo, BlockInfo>>> _blocks):
-            depth(_depth),
-            infos(_infos),
-            blocks(_blocks) { };
+    class simple_role_choicer: public role_choicer {
 
+        const std::shared_ptr<arbac_policy>& policy;
 
+    public:
+        explicit simple_role_choicer(const std::shared_ptr<arbac_policy>& _policy) :
+                policy(_policy) { }
 
-    template <typename LayerInfo, typename BlockInfo>
-    block<LayerInfo, BlockInfo>::block(int _depth,
-                 std::shared_ptr<BlockInfo> _infos,
-                 std::shared_ptr<layer<LayerInfo, BlockInfo>> _refinement_layer):
-            depth(_depth),
-            infos(_infos),
-            refinement_layer(_refinement_layer) { };
+        Choice classify(atomp r) const override {
+//            if (r->name == "r1" || r->name == "r2") {
+//                return REQUIRED;
+//            }
+            return FREE;
+        }
+        int get_required_count() const override {
+            int count = 0;
+            for (auto &&atom :policy->atoms()) {
+                if (classify(atom) == REQUIRED) {
+                    count++;
+                }
+            }
+            return count;
+        }
 
-    bool block<LayerInfo, BlockInfo>::is_leaf() {
-        return refinement_layer == nullptr;
     };
 
-    template <typename BlockSolverInfo>
-    simple_block_info<BlockSolverInfo>::simple_block_info(std::vector<rulep> _rules,
-                                         Expr _invariant,
-                                         std::shared_ptr<BlockSolverInfo> _solverInfo):
-        rules(_rules),
-        invariant(_invariant),
-        solverInfo(_solverInfo) { }
+    class learning_skeleton {
+    private:
+        const std::shared_ptr<arbac_policy> policy;
+//        const bool use_admin;
+        const overapprox_strategy strategy;
+        const role_choicer _role_choicer;
+
+        class b_solver_info {
+        };
+
+        class l_solver_info {
+        };
+
+        typedef std::shared_ptr<glayer<simple_layer_info<l_solver_info>, simple_block_info<b_solver_info>>> layer;
+        typedef std::shared_ptr<gblock<simple_layer_info<l_solver_info>, simple_block_info<b_solver_info>>> block;
+
+        struct layer_restriction_info {
+//            std::set<atomp> in_adm_target;
+//            std::set<atomp> in_adm_precs;
+//            std::set<rulep> assigning_adm;
+            std::set<atomp> in_reg_target;
+            std::set<atomp> in_reg_precs;
+            std::set<rulep> assigning_reg;
+
+            layer_restriction_info():
+//                    in_adm_target(std::set<atomp>()),
+//                    in_adm_precs(std::set<atomp>()),
+//                    assigning_adm(std::set<rulep>()),
+                    in_reg_target(std::set<atomp>()),
+                    in_reg_precs(std::set<atomp>()),
+                    assigning_reg(std::set<rulep>()) { }
+        };
+
+        layer_restriction_info get_required(const std::shared_ptr<arbac_policy> &policy,
+                                            const std::vector<std::pair<Expr, Expr>> &target_exprs) {
+            layer_restriction_info res;
+            for (auto &&expr : target_exprs) {
+//                if (use_admin) {
+//                    res.in_adm_target.insert(expr.second->atoms().begin(), expr.second->atoms().end());
+//                }
+                for (auto &&atom : expr.first->atoms()) {
+                    if (_role_choicer.classify(atom) != role_choicer::EXCLUDED) {
+                        res.in_reg_target.insert(atom);
+                    }
+                }
+            }
+
+            for (auto &&rule : policy->rules()) {
+                if (contains(res.in_reg_target, rule->target)) {
+                    res.assigning_reg.insert(rule);
+//                    if (use_admin) {
+//                        res.in_precs.insert(rule->admin->atoms().begin(), rule->admin->atoms().end());
+//                    }
+                    res.in_reg_precs.insert(rule->prec->atoms().begin(), rule->prec->atoms().end());
+                }
+            }
+
+            return res;
+        };
+
+        int get_layer_block_count(const std::shared_ptr<arbac_policy>& policy,
+                                  const layer_restriction_info& infos,
+                                  overapprox_strategy strategy) {
+            if (strategy.block_count > 0) {
+                return strategy.block_count;
+            } else {
+                int requireds = _role_choicer.get_required_count();
+                int dynamic = (int) infos.in_reg_target.size();
+
+                return requireds + dynamic;
+            }
+        };
+
+        layer create_from_policy(std::shared_ptr<arbac_policy> policy, overapprox_strategy strategy) {
 
 
+            return nullptr;
+        };
 
-    template <typename LayerSolverInfo>
-    simple_layer_info<LayerSolverInfo>::simple_layer_info(int _block_count,
-                                         bool _require_nondet_block,
-                                         std::vector<rulep> _rules,
-                                         Expr _invariant,
-                                         std::shared_ptr<LayerSolverInfo> _solverInfo):
-            block_count(_block_count),
-            require_nondet_block(_require_nondet_block),
-            rules(_rules),
-            invariant(_invariant),
-            solverInfo(_solverInfo) { }
+    public:
+
+        learning_skeleton(const std::shared_ptr<arbac_policy> _policy,
+                          const bool _use_admin,
+                          const overapprox_strategy _strategy):
+                policy(_policy),
+//                use_admin(_use_admin),
+                strategy(_strategy),
+                role_choicer(simple_role_choicer(policy)) { };
+
+    }
 
 }
--- src/VACSAT/src/over_skeleton.h
+++ src/VACSAT/src/over_skeleton.h
@@ -11,40 +11,64 @@
 
 namespace SMT {
 
-/*
-template <typename LayerInfo, typename BlockInfo>
-class node<LayerInfo, BlockInfo> {
-
-};*/
-
-    template <typename LayerInfo, typename BlockInfo>
-    class block;
-
-    template <typename LayerInfo, typename BlockInfo>
-    class layer {//: public node<LayerInfo, BlockInfo> {
+    class role_choicer {
     public:
+        enum Choice {
+            REQUIRED,
+            FREE,
+            EXCLUDED
+        };
+        virtual Choice classify(atomp r) const = 0;
+        virtual int get_required_count() const = 0;
+    };
+
+    typedef std::list<int> tree_path;
+
+    struct overapprox_strategy {
+        int max_depth;
+        int block_count;
+    };
+
+    template <typename LayerInfo, typename BlockInfo>
+    class gblock;
+
+    template <typename LayerInfo, typename BlockInfo>
+    class glayer {//: public node<LayerInfo, BlockInfo> {
+    public:
+        tree_path path;
+        int uid;
         int depth;
         std::shared_ptr<LayerInfo> infos;
-        std::vector<std::shared_ptr<block<LayerInfo, BlockInfo>>> blocks;
+        std::vector<std::shared_ptr<gblock<LayerInfo, BlockInfo>>> blocks;
 
-        layer(int _depth,
-              std::shared_ptr<LayerInfo> _infos,
-              std::vector<std::shared_ptr<block<LayerInfo, BlockInfo>>> _blocks):
+        glayer(tree_path _path,
+               int _uid,
+               int _depth,
+               std::shared_ptr<LayerInfo> _infos,
+               std::vector<std::shared_ptr<gblock<LayerInfo, BlockInfo>>> _blocks):
+                path(_path),
+                uid(_uid),
                 depth(_depth),
                 infos(_infos),
                 blocks(_blocks) { }
     };
 
     template <typename LayerInfo, typename BlockInfo>
-    class block {//: public node<LayerInfo, BlockInfo> {
+    class gblock {//: public node<LayerInfo, BlockInfo> {
     public:
+        tree_path path;
+        int uid;
         int depth;
         std::shared_ptr<BlockInfo> infos;
-        std::shared_ptr<layer<LayerInfo, BlockInfo>> refinement_layer;
+        std::shared_ptr<glayer<LayerInfo, BlockInfo>> refinement_layer;
 
-        block(int _depth,
-              std::shared_ptr<BlockInfo> _infos,
-              std::shared_ptr<layer<LayerInfo, BlockInfo>> _refinement_layer):
+        gblock(tree_path _path,
+               int _uid,
+               int _depth,
+               std::shared_ptr<BlockInfo> _infos,
+               std::shared_ptr<glayer<LayerInfo, BlockInfo>> _refinement_layer):
+                path(_path),
+                uid(_uid),
                 depth(_depth),
                 infos(_infos),
                 refinement_layer(_refinement_layer) { }
@@ -55,44 +79,50 @@ class node<LayerInfo, BlockInfo> {
         }
     };
 
+    template <typename LayerSolverInfo>
+    class simple_layer_info {
+    public:
+        const std::shared_ptr<arbac_policy>& policy;
+        int block_count;
+        bool require_nondet_block;
+        std::vector<rulep> rules;
+        Expr target_rule;
+        Expr invariant;
+        std::shared_ptr<LayerSolverInfo> solverInfo;
+
+        simple_layer_info(std::shared_ptr<arbac_policy>& _policy,
+                          int _block_count,
+                          bool _require_nondet_block,
+                          std::vector<rulep> _rules,
+                          Expr _target_rule,
+                          Expr _invariant,
+                          std::shared_ptr<LayerSolverInfo> _solverInfo):
+                policy(_policy),
+                block_count(_block_count),
+                require_nondet_block(_require_nondet_block),
+                rules(_rules),
+                target_rule(_target_rule),
+                invariant(_invariant),
+                solverInfo(_solverInfo) { }
+    };
+
     template <typename BlockSolverInfo>
     class simple_block_info {
     public:
+        const std::shared_ptr<arbac_policy>& policy;
         std::vector<rulep> rules;
         Expr invariant;
         std::shared_ptr<BlockSolverInfo> solverInfo;
 
-        simple_block_info(std::vector<rulep> _rules,
-                          Expr _invariant,
-                          std::shared_ptr<BlockSolverInfo> _solverInfo):
-                rules(_rules),
-                invariant(_invariant),
-                solverInfo(_solverInfo) { }
-    };
-
-    template <typename LayerSolverInfo>
-    class simple_layer_info {
-    public:
-        int block_count;
-        bool require_nondet_block;
-        std::vector<rulep> rules;
-        Expr invariant;
-        std::shared_ptr<LayerSolverInfo> solverInfo;
-
-        simple_layer_info(int _block_count,
-                          bool _require_nondet_block,
+        simple_block_info(std::shared_ptr<arbac_policy>& _policy,
                           std::vector<rulep> _rules,
                           Expr _invariant,
-                          std::shared_ptr<LayerSolverInfo> _solverInfo):
-                block_count(_block_count),
-                require_nondet_block(_require_nondet_block),
+                          std::shared_ptr<BlockSolverInfo> _solverInfo):
+                policy(_policy),
                 rules(_rules),
                 invariant(_invariant),
                 solverInfo(_solverInfo) { }
     };
-
-
-
 }
 
 #endif //VACSAT_OVER_SKELETON_H
